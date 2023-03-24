46960c87e0cc1f9941223e1eb218ad46095f5c81
Kidus-19 <105101940+Kidus-19@users.noreply.github.com>
2023-03-25 00:43:43 +0300

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states Order By states.id")
     rows = cur.fetchall()
 
     for row in rows:
