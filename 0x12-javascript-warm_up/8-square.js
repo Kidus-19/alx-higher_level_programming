3fbb69ff280abe6acb5be5364028d314f9077759
Kidus-19 <kidusfesseha@gmail.com>
2023-11-13 14:59:50 -0800

    8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// print a square using X by the first argument.
+
+const size = process.argv[2];
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < parseInt(size); i++) {
+    console.log('X'.repeat(parseInt(size)));
+  }
+}
