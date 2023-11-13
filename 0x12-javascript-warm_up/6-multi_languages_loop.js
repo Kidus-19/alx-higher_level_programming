7d0b7152c839b61fdd2ee42f6620e29f4b5c14fb
Kidus-19 <kidusfesseha@gmail.com>
2023-11-13 14:14:00 -0800

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,6 @@
 // prints 3 lines by using an array of string and a loop
 
 const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < 3; i++){
-	console.log(lines[i]);
+for (let i = 0; i < 3; i++) {
+  console.log(lines[i]);
 }
