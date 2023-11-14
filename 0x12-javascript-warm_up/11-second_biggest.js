e7b470e43837ff727ea445004343db1eb5fdfeab
Kidus-19 <kidusfesseha@gmail.com>
2023-11-13 16:18:55 -0800

    bug fix on 2nd largest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,8 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const numbers = process.argv.sort();
-  console.log(numbers.reverse()[1]);
+  const numbers = process.argv.slice(2).map(Number);
+  const uniqueNumbers = Array.from(new Set(numbers));
+  const sortedNumbers = uniqueNumbers.sort((a, b) => b - a);
+  console.log(sortedNumbers[1]);
 }
